da67f7a31ccdb120aa05b531a83310f93c565f19
HoseinDarvishi <HosseinDarvishi138@gmail.com>
2025-11-22 00:52:21 +0330

    Implementing Product Models

---
+++ Tifan.Product/Infra/ProductDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tifan.Product.Infra
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Models.Product> Products { get;set; }
+        public DbSet<Models.Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            base.OnModelCreating(mb);
+        }
+    }
+}
---
+++ Tifan.Product/Models/Category.cs
@@ -0,0 +1,62 @@
+﻿namespace Tifan.Product.Models
+{
+    public class Category
+    {
+        private Category() {}
+
+        public Category(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public ICollection<Product> Products { get; set; }
+
+        public void Edit(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+
+    public class Product 
+    {
+        private Product()
+        {
+            
+        }
+
+        public Product(string name, string description, string image, decimal price, Guid categoryId)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+            CategoryId = categoryId;
+            CreateDate = DateTime.Now;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Guid CategoryId { get; private set; }
+        public string Image { get; private set; }
+        public decimal Price { get; private set; }
+        public DateTime CreateDate { get; set; }
+
+        public Category Category { get; private set; }
+
+
+        public void Edit(string name, string description, string image, decimal price, Guid categoryId)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+            CategoryId = categoryId;
+        }
+    }
+}
