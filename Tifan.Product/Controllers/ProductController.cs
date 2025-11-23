a632848747dd3a2875487a2d875cb8ef37a14763
HoseinDarvishi <HosseinDarvishi138@gmail.com>
2025-11-23 18:14:21 +0330

    adding Product Controller (Product Project)

---
+++ Tifan.Product/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tifan.Product.IServices;
+using Tifan.Product.Wrappers.Product;
+
+namespace Tifan.Product.Controllers;
+
+[Route("Tifan/Api/[Product]")]
+[ApiController]
+public class ProductController(IProductService _service) : Controller
+{
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> Get(Guid id, CancellationToken ct)
+    {
+        var product = await _service.GetAsync(id, ct);
+        return Ok(product);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll(CancellationToken ct)
+    {
+        var products = await _service.GetAllAsync(ct);
+        return Ok(products);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateProduct product)
+    {
+        var productId = await _service.AddAsync(product);
+        return Ok(productId);
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> Edit([FromBody] EditProduct product)
+    {
+        await _service.UpdateAsync(product);
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Remove(Guid id)
+    {
+        await _service.RemoveAsync(id);
+        return Ok();
+    }
+}
--- Tifan.Product/Program.cs
+++ Tifan.Product/Program.cs
@@ -1,6 +1,7 @@
 using Mapster;
 using Microsoft.EntityFrameworkCore;
 using Tifan.Product.Infra;
+using Tifan.Product.IServices;
 using Tifan.Product.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,9 @@ builder.Services.AddDbContext<ProductDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("TifanProductDatabase")
     ));
+
+builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.AddTransient<ICategoryService, CategoryService>();
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
