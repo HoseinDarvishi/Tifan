ce383134cbf12c4d34f009471a0b94e42abf7f79
HoseinDarvishi <HosseinDarvishi138@gmail.com>
2025-11-29 19:23:46 +0330

    adding basket service Project + Basket / BasketItem Models

--- Tifan.AppHost/AppHost.cs
+++ Tifan.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Tifan_Product>("tifan-product");
 
+builder.AddProject<Projects.Tifan_Basket>("tifan-basket");
+
 builder.Build().Run();
---
+++ Tifan.Basket/Models/Basket.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TifanBasket.Models;
+
+public class Basket
+{
+    private Basket() { }
+    public Basket(string userId)
+    {
+        UserId = userId;
+    }
+
+    public Guid Id { get; set; }
+    public string UserId { get; private set; }
+
+    public ICollection<BasketItem> Items { get; set; }
+}
+
+public class BasketItem
+{
+    private BasketItem() { }
+
+    public BasketItem(Guid basketId, Guid productId, string productName, string unitPrice, int quantity, string imageUrl)
+    {
+        BasketId = basketId;
+        ProductId = productId;
+        ProductName = productName;
+        UnitPrice = unitPrice;
+        Quantity = quantity;
+        ImageUrl = imageUrl;
+    }
+
+    public Guid BasketId { get; private set; }
+    public Guid ProductId { get; private set; }
+    public string ProductName { get; private set; }
+    public string UnitPrice { get; private set; }
+    public int Quantity { get; private set; }
+    public string ImageUrl { get; private set; }
+
+    [ForeignKey(nameof(BasketId))]
+    public Basket Basket { get; private set; }
+
+
+    public void SetQuantity(int quantity)
+    {
+        Quantity = quantity;
+    }
+}
