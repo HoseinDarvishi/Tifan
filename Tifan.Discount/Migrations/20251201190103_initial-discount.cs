6ad7a7b966a3b754f9aa04fd2f00f0cae57d703d
HoseinDarvishi <HosseinDarvishi138@gmail.com>
2025-12-01 22:31:37 +0330

    adding migration (Discount Project)

---
+++ Tifan.Discount/Migrations/20251201190103_initial-discount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tifan.Discount.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialdiscount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Discounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsExpired = table.Column<bool>(type: "bit", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discounts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Discounts");
+        }
+    }
+}
