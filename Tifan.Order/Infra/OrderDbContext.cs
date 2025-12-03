5bed4c3266bc81f5f33138c143f683b676d82d90
HoseinDarvishi <HosseinDarvishi138@gmail.com>
2025-12-03 22:07:20 +0330

    adding dbContext (Order Project)

---
+++ Tifan.Order/Infra/OrderDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tifan.Order.Models;
+
+namespace Tifan.Order.Infra
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tifan.Order.Models.Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Tifan.Order/Program.cs
+++ Tifan.Order/Program.cs
@@ -1,18 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Tifan.Order.Infra;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<OrderDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("TifanOrderDatabase")
+    ));
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
