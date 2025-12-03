9eac3698150ef84ca2b7b4742d6f31fb84cc57cd
HoseinDarvishi <HosseinDarvishi138@gmail.com>
2025-12-03 22:01:04 +0330

    adding models (Order Project)

---
+++ Tifan.Order/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace Tifan.Order.Models;
+
+public class Order
+{
+    private Order() { }
+
+    public Order(string userId, List<OrderItem> items)
+    {
+        UserId = userId;
+        OrderPlaced = DateTime.Now;
+        OrderItems = items;
+        OrderPaid = false;
+    }
+
+    public Guid Id { get; private set; }
+    public string UserId { get; private set; }
+    public DateTime OrderPlaced { get; private set; }
+    public bool OrderPaid { get; private set; }
+    public ICollection<OrderItem> OrderItems { get; private set; }
+}
+
---
+++ Tifan.Order/Models/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tifan.Order.Models;
+
+public class OrderItem 
+{
+    public Guid Id { get; private set; }
+    public Guid OrderId { get; set; }
+
+    [ForeignKey(nameof(OrderId))]
+    public Order Order { get; set; }
+}
+
